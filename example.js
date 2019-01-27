cce624ac6b3b4ead833185fcc4f896762b88f735
jiangplus <jiang@jiang.com>
2019-01-27 16:23:10 +0800

    init project

---
+++ example.js
@@ -0,0 +1,16 @@
+let base32 = require('./lib/base32')
+
+let encoded = base32.encode('some data to encode')
+let decoded = base32.decode(encoded)
+console.log(encoded, decoded)
+
+1548576152614
+1548576164021
+1548576215386
+
+
+
+
+
+
+
